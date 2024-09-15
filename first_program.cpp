ca8b38dc50ecc78c70b3b777038cd6434156310c
SUMIT MISHRA <93571941+sumitmishra0@users.noreply.github.com>
2024-09-15 06:56:03 +0530

    Create first_program.cpp

---
+++ first_program.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	// char main = 'c';
+	// cout << main;
+	// char int = 'd';
+	// char char = 'd';
+
+	// char endl = 'a';
+	// cout << endl;
+
+	// char ch = 234234;
+	// cout << ch << endl;
+
+	// short a = 2^16 -1;
+	// short b = 10;
+	// short sum = a+b;
+	// cout << sum << endl;
+
+	// int a = 5;
+	// char b = 'd';
+	// int sum1 = a + b;
+	// float f = 2.1 + sum1;
+	// cout << f;
+
+	// char a = 5;
+	// char b = 'd';
+	// char sum1 = a + b;
+
+
+//   float f = 2.7;
+//   int n = 157;
+
+//   int diff = n - f;
+//   cout << diff;
+
+int sum = 5;
+float f = 2.1;
+
+
+// float value = sum/f;
+// cout << value;
+
+int value1 = sum/f;
+cout << value1;
+
+	// cout << sum1;
+	return 0;
+}
